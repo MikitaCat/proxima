aa705c4a791509b4e8497ef02641b37a5fdfd9ca
Mikita Stankevich <stankevich.mikita21@gmail.com>
2023-11-30 23:24:54 +0100

    RootLayout updates

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Proxima",
+  description:
+    "Mikita's independent initiative crafted with Next.js 14. Itemulates specific features of Trello™. The core motivation behind this project is to explore the depths of Next.js 14 while constructing a demonstrative portfolio that will enthrall potential employers and highlight my adeptness in this technology.",
+};
